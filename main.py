6db12bf3241b8f435eff9dcda08dc969ee7ab602
Lukas Stefancik <xtefancik@gstuba.sk>
2023-11-04 21:23:15 +0100

    1.2 final optimalization

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
+import time
 import random
+import pandas
 import matplotlib.pyplot as plt
 from copy import deepcopy
 
@@ -13,34 +15,29 @@ def generation(n):
     return gen
 
 
-def plot_start(path):
-    x, y = zip(*path)
+def plot_start(x, y):
     plt.ion()
-    plt.subplots(1, 2, figsize=(12, 6))
-
-    plt.subplot(1, 2, 1)
-    plt.scatter(x, y)
-    plt.plot(x, y, color='red')
-
-    plt.subplot(1, 2, 2)
-    plt.plot(x, y, color='red')
-
-    plt.show()
+    plt.figure(figsize=(x, y))
 
 
 def plot_update(path, data):
     plt.clf()
 
     x, y = zip(*path)
-    plt.subplot(1, 2, 1)
+    plt.subplot(2, 2, 1)
     plt.scatter(x, y)
     plt.plot(x, y, color='red')
 
-    x, y = zip(*data)
-    plt.subplot(1, 2, 2)
-    plt.plot(x, y, color='red')
+    x, y1, y2, y3 = zip(*data)
+    plt.subplot(2, 2, 2)
+    plt.plot(x, y1, color='red')
 
-    plt.pause(0.005)
+    plt.subplot(2, 2, 3)
+    plt.plot(x, y2, color='red')
+
+    plt.subplot(2, 2, 4)
+    plt.plot(x, y3, color='red')
+    plt.pause(0.01)
     plt.draw()
 
 
@@ -51,36 +48,42 @@ def distance(path):
     return dist
 
 
-def neighborhood(path):
-    neighbors = []
-    for i in range(1, len(path) - 1):
-        for j in range(i + 1, len(path) - 1):
-            inversion = deepcopy(path)
-            inversion[i], inversion[j] = inversion[j], inversion[i]
-            neighbors.append(inversion)
-    return neighbors
+def tabu(path, max_threshold=25, tabu_size=25, plotting=True):
+    def neighborhood_swap(entry):
+        neighbor_list = []
 
+        for i in range(1, len(entry) - 1):
+            for j in range(i + 1, len(entry) - 1):
+                swap = deepcopy(entry)
+                swap[i], swap[j] = swap[j], swap[i]
+                neighbor_list.append(swap)
 
-def random_neighbor(path):
-    new_path = deepcopy(path)
-    x, y = 0, 0
-    while x == y:
-        x = random.randint(1, len(path)-2)
-        y = random.randint(1, len(path)-2)
+        return neighbor_list
 
-    new_path[x], new_path[y] = new_path[y], new_path[x]
-    return new_path
+    def neighborhood_2opt(entry):
+        neighbor_list = []
 
+        for i in range(1, len(entry) - 2):
+            for j in range(i + 2, len(entry)):
+                opt = entry[:i] + entry[i:j][::-1] + entry[j:]
+                neighbor_list.append(opt)
 
-def tabu(path, tabu_size, max_threshold):
-    data = []
-    tabu_list = [path]
-    best_solution, best_candidate = path, path
-    plot_start(path)
+        return neighbor_list
 
     threshold = 0
+    iteration = 0
+
+    tabu_list = [path]
+    best_solution, best_candidate = path, path
+    data = []
+
+    if plotting:
+        plot_start(8, 8)
     while threshold < max_threshold:
-        neighbors = neighborhood(best_candidate)
+        if random.random() > 0.5:
+            neighbors = neighborhood_swap(best_candidate)
+        else:
+            neighbors = neighborhood_2opt(best_candidate)
         best_candidate = neighbors[0]
 
         for neighbor in neighbors:
@@ -96,29 +99,90 @@ def tabu(path, tabu_size, max_threshold):
         if len(tabu_list) > tabu_size:
             tabu_list.pop(0)
 
-        data.append((len(data) + 1, distance(best_solution)))
-        plot_update(best_solution, data)
-        print(f"Iteration: {len(data)} Distance: {distance(best_solution)} Tabu list occupation: {len(tabu_list)} Threshold: {threshold}")
+        iteration += 1
+        if plotting:
+            data.append((len(data) + 1, distance(best_solution), len(tabu_list), threshold))
+            plot_update(best_solution, data)
+            print(f"Iteration: {iteration} Distance: {distance(best_solution)} Tabu list occupation: {len(tabu_list)} Threshold: {threshold}")
 
-    plt.pause(10)
+    return distance(best_solution)
 
 
-def simulated_annealing(path, max_treshold, max_one_temp):
+def simulated_annealing(path, alpha=0.995, temperature=3, plotting=True):
+    def random_neighbor_swap(entry):
+        new_path = deepcopy(entry)
+        x, y = 0, 0
+        while x == y:
+            x = random.randint(1, len(entry) - 2)
+            y = random.randint(1, len(entry) - 2)
+
+        new_path[x], new_path[y] = new_path[y], new_path[x]
+        return new_path
+
+    def random_neighbor_shuffle(entry):
+        new_path = deepcopy(entry)
+        x, y = 0, 0
+        while x == y:
+            x = random.randint(1, len(entry) - 2)
+            y = random.randint(1, len(entry) - 2)
+
+        shuffle = new_path[min(x, y):max(x, y) + 1]
+        random.shuffle(shuffle)
+        return new_path[:min(x, y)] + shuffle + new_path[max(x, y) + 1:]
+
+    def random_neighbor_inverse(entry):
+        new_path = deepcopy(entry)
+        x, y = 0, 0
+        while x == y:
+            x = random.randint(1, len(entry) - 2)
+            y = random.randint(1, len(entry) - 2)
+
+        new_path[min(x, y):max(x, y) + 1] = new_path[min(x, y):max(x, y) + 1][::-1]
+        return new_path
+
+    def random_neighbor_insert(entry):
+        new_path = deepcopy(entry)
+        x, y = 0, 0
+        while x == y:
+            x = random.randint(1, len(entry) - 2)
+            y = random.randint(1, len(entry) - 2)
+
+        temp = new_path[x]
+        new_path.pop(x)
+        new_path.insert(y, temp)
+        return new_path
+
     threshold = 0
-    temperature = 5
-    data = []
-    best_solution = path
+    iteration = 0
 
-    plot_start(path)
-    while threshold < max_treshold:
-        candidate = random_neighbor(best_solution)
+    max_threshold = 8 * len(path)
+    max_one_temp = 18 * len(path)
+
+    best_solution = path
+    data = []
+
+    if plotting:
+        plot_start(8, 8)
+
+    while threshold < max_threshold:
+        choice = random.choice([1, 2, 3, 4])
+
+        if choice == 1:
+            candidate = random_neighbor_swap(best_solution)
+        elif choice == 2:
+            candidate = random_neighbor_insert(best_solution)
+        elif choice == 3:
+            candidate = random_neighbor_shuffle(best_solution)
+        else:
+            candidate = random_neighbor_inverse(best_solution)
+
         delta = distance(best_solution) - distance(candidate)
 
         for i in range(max_one_temp):
             if delta > 0:
                 best_solution = candidate
                 threshold = 0
-            else:
+            elif delta < 0:
                 prob = math.exp(delta/temperature)
                 if random.random() < prob:
                     best_solution = candidate
@@ -128,13 +192,29 @@ def simulated_annealing(path, max_treshold, max_one_temp):
             if data[-1][1] == distance(best_solution):
                 threshold += 1
 
-        temperature *= 0.995
-        data.append((len(data) + 1, distance(best_solution)))
-        plot_update(best_solution, data)
-        print(f"Iteration: {len(data)} Distance: {distance(best_solution)} Temperature: {temperature} Threshold: {threshold}")
+        temperature *= alpha
+        iteration += 1
+        data.append((len(data) + 1, distance(best_solution), temperature, threshold))
+
+        if plotting:
+            plot_update(best_solution, data)
+            print(f"Iteration: {len(data)} Distance: {distance(best_solution)} Temperature: {temperature} Threshold: {threshold}")
+
+    return distance(best_solution)
 
 
-start = generation(40)
-# start = [(60, 200), (180, 200), (100, 180), (140, 180), (20, 160), (80, 160), (200, 160), (140, 140), (40, 120), (120, 120), (180, 100), (60, 80), (100, 80), (180, 60), (20, 40), (100, 40), (200, 40), (20, 20), (60, 20), (160, 20)]
-tabu(start, 100, 100)
-simulated_annealing(start, 100, 10000)
+def test(n, algorithm):
+    sum = 0
+    for i in range(n):
+        start = generation(20)
+        time_start = time.time()
+        algorithm(start, plotting=False)
+        time_end = time.time()
+        stop = time_end - time_start
+        print(stop)
+        sum += stop
+
+    print(sum/n)
+
+
+test(50, tabu)
